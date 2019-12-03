72658e6a16be7005624fa51004f6cd6e7ce7fde4
TseIvan <itse@ualberta.ca>
2019-12-03 15:55:44 -0700

    fin d2

--- adventcode.py
+++ adventcode.py
@@ -1,3 +1,8 @@
+import copy
+
+OP_ADD = 1
+OP_MUL = 2
+OP_END = 99
 
 def day1(f1='adventcode.txt',p2 = False):
     with open(f1, 'r') as fp:
@@ -17,13 +22,15 @@ def day1(f1='adventcode.txt',p2 = False):
                 summation += (int(line)//3 -2)
     return summation
 
-def opcodeProgram(opcode) -> list:
+def opcodeProgram(x,y,opcode) -> list:
     # Opcode 1 = Add, retrieve(index) and retrieve(index), add to pos at index+3
     # Opcode 2 = Multiplies, retrieve(index) and retrieve(index), add to pos at index+3
     # Opcode 99 = Halt
     # Rest = something went wrong
+    opcode = copy.deepcopy(opcode)
+    opcode[1], opcode[2] = x,y
     index = 0
-    while (index <= len(opcode) and opcode[index] != 99):
+    while (opcode[index] != 99):
         if opcode[index] == 1:
             opcode[opcode[index+3]] = opcode[opcode[index+1]] + opcode[opcode[index+2]]
         elif opcode[index] == 2:
@@ -31,7 +38,7 @@ def opcodeProgram(opcode) -> list:
         index += 4
     return opcode
 
-def day2part1(f1="day2.txt"):
+def day2(f1="day2.txt", p2 = False):
     # print(opcodeProgram([1,9,10,3,2,3,11,0,99,30,40,50]) == [3500,9,10,70,2,3,11,0,99,30,40,50])
     # print(opcodeProgram([1,0,0,0,99]) == [2,0,0,0,99])
     # print(opcodeProgram([2,3,0,3,99]) == [2,3,0,6,99])
@@ -44,14 +51,26 @@ def day2part1(f1="day2.txt"):
         for char in line.rstrip('\n').split(","):
             if char.isdigit():
                 opcode.append(int(char))
+
     # #To do this, before running the program, replace position 1 with the value 12 and replace position 2 with the value 2. What value is left at position 0 after the program halts?
-    opcode[1], opcode[2] = 12,2
-    return opcodeProgram(opcode)[0]
+    if not p2:
+        return opcodeProgram(12,2,opcode)[0]
+    # "With terminology out of the way, we're ready to proceed. To complete the gravity assist, you need to determine what pair of inputs produces the output 19690720."
+    # Find the input noun and verb that cause the program to produce the output 19690720. What is 100 * noun + verb? (For example, if noun=12 and verb=2, the answer would be 1202.)
+    else:
+        # while (opcodeProgram(opcode)[0] != 19690720):
+        opcode = copy.deepcopy(opcode)
+        for x in range(0,100):
+            for y in range(0,100):
+                if opcodeProgram(x,y,opcode)[0] == 19690720:
+                    return 100 * x + y
+        return -1
 
 def main():
     # print(day1())
     # print(day1(p2 = True))
-    # print(day2part1())
+    print(day2())
+    print(day2(p2 = True))
     return 0
 
 
