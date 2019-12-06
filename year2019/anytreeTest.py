d97108aeff8559dccddb632e3365e975e71530f3
TseIvan <itse@ualberta.ca>
2019-12-06 00:49:53 -0700

    fiinished day7p1

--- year2019/anytreeTest.py
+++ year2019/anytreeTest.py
@@ -1,4 +1,4 @@
-from anytree import Node, RenderTree, AsciiStyle
+from anytree import Node, RenderTree, AsciiStyle, LevelOrderIter, Walker
 # python library anytree: https://pypi.org/project/anytree/
 # Could be done strictly using dictionary but I'm trying out anytree for fun visualization
 # Documentation - https://anytree.readthedocs.io/en/latest/#
@@ -20,27 +20,24 @@ def day7(f1="day7.txt", p2 = False):
     tracking = {}
     for planet in orbit_list:
         if len(planet) == 2:
-
             if planet[0] not in tracking and planet[1] not in tracking:
                 x = Node(planet[0])
                 y = Node(planet[1], parent=x)
-
                 tracking[planet[0]] = x
                 tracking[planet[1]] = y
-
             elif planet[0] not in tracking and planet[1] in tracking:
                 x = Node(planet[0])
                 tracking[planet[0]] = x
                 tracking[planet[1]].parent = x
-
             elif planet[0] in tracking and planet[1] not in tracking:
                 x = Node(planet[1], parent=tracking[planet[0]])
                 tracking[planet[1]] = x
+            else:
+                tracking[planet[1]].parent = tracking[planet[0]]
 
-                
-    # max = 0
-    # for i in tracking:
-    #     print(RenderTree(tracking[i], style=AsciiStyle()))
-    # for pre, fill, node in RenderTree(tracking['COM']):
-    #     print("%s%s" % (pre, node.name))
+    indirect_orbit = 0
+    for k,v in tracking.items():
+        indirect_orbit += len(v.ancestors)
+    print(indirect_orbit)
+
 day7()
