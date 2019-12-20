c43586546dfa96a8e34ea40938a65acf54da0f34
TseIvan <itse@ualberta.ca>
2019-12-19 19:36:32 -0700

    day12p1

--- year2019/day12.py
+++ year2019/day12.py
@@ -1,5 +1,48 @@
-def main():
+import re
+class Moon:
+    def __init__(self,pos):
+        self.x = pos[0]
+        self.y = pos[1]
+        self.z = pos[2]
+        self.velo_x = 0
+        self.velo_y = 0
+        self.velo_z = 0
 
+    def __str__(self):
+        return "<%s,%s,%s> <%s,%s,%s>" % (self.x,self.y,self.z,self.velo_x,self.velo_y,self.velo_z)
+
+    def getXYZ(self):
+        return [self.x,self.y,self.z]
+
+    def compute_gravity(self,other):
+        if self.x != other[0]:
+            self.velo_x = self.velo_x-1 if self.x > other[0] else self.velo_x+1
+        if self.y != other[1]:
+            self.velo_y = self.velo_y-1 if self.y > other[1] else self.velo_y+1
+        if self.z != other[2]:
+            self.velo_z = self.velo_z-1 if self.z > other[2] else self.velo_z+1
+
+    def update(self):
+        self.x += self.velo_x
+        self.y += self.velo_y
+        self.z += self.velo_z
+
+    def compute_energy(self):
+        return sum([abs(self.x),abs(self.y),abs(self.z)]) * sum([abs(self.velo_x),abs(self.velo_y),abs(self.velo_z)])
+
+def main():
+    Moons = []
+    with open("day12.txt", 'r') as fp:
+        for _ in [list(map(int,re.findall("[-?\d+]+",line))) for line in fp.readlines()]:
+            Moons.append(Moon(_))
+    for _ in range(1000):
+        for x in Moons:
+            for y in Moons:
+                if x!=y:
+                    x.compute_gravity(y.getXYZ())
+        for z in Moons:
+            z.update()
+    print(sum(p.compute_energy() for p in Moons))
     return
 if __name__ == "__main__":
     main()
