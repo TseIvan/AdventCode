a192c7db37c0f41ed12ddb2c5913372b6050934d
TseIvan <itse@ualberta.ca>
2019-12-20 11:59:39 -0700

    finished day16 part a, need 2 compute  pattern on b

--- year2019/day16.py
+++ year2019/day16.py
@@ -1,19 +1,31 @@
-def compute_pattern(index) -> list:
-    pattern = [0, 1, 0, -1]
-    output_pattern = []
-    for i in pattern:
-        output_pattern += index*[i]
-    output_pattern.pop(0)
-    return output_pattern
+import numpy as np
+import math
 
 def compute_signal():
     with open("day16.txt", 'r') as fp:
-        signal = [int(x) for x in fp.read().rstrip('\n')]
+        signal = np.array([int(x) for x in fp.read().rstrip('\n')])
 
-    new_signal = []
+    offset = int("".join([str(x) for x in (signal[:7]).tolist()]))
+    repeat_pattern = [0, 1, 0, -1]
+    offset_applied = False
+    iter = 0
+    while(iter < 10000):
+        new_signal = []
+        for i in range(signal.size): # Loops 8 times
+            pattern = (np.repeat(repeat_pattern,i+1).tolist()) # Pattern shifted left by one
+            pattern = np.array((pattern*math.ceil(signal.size/(len(pattern)-1)))[1:signal.size+1])
+            new_signal.append(str(repr(np.sum(np.multiply(signal,pattern)))[-1]))
 
+        if len(new_signal) > offset and offset_applied == False:
+            offset_applied = True
+            repeat_pattern = [0,0,1,0,-1]
+            signal[offset:]
+        else:
+            signal = np.array([int(x) for x in new_signal])
+        if iter%1000: # sanity check
+            print("1000 Done",flush=True)
+        iter += 1
+    print(signal[:10],flush=True)
     return
-
-# print(compute_pattern(1))
 
 compute_signal()
