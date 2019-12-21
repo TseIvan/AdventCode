f9d35c08ec062cd1a110a154da0db0d46916383c
TseIvan <itse@ualberta.ca>
2019-12-21 00:25:07 -0700

    figuring out d10 p2

--- year2019/day10.py
+++ year2019/day10.py
@@ -1,4 +1,5 @@
 import numpy as np
+from collections import defaultdict
 import math
 # Cannot use Arctan eg 0,0 and -1,-1 rets 1. 0.0 and 1.1 rets 1. Both are the same
 # Use atan2 to compute unique slopes https://en.wikipedia.org/wiki/Atan2
@@ -9,16 +10,22 @@ class Asteroid:
         self.y = y
         self.unique = set()
         self.max = -math.inf
-
+        self.distance = -math.inf
 
     def __str__(self):
         return "x_coordinate %s, y_coordinate %s" %(self.x,self.y)
 
     def slope(self,other):
-        self.unique.add(math.atan2((self.y - other.y),(self.x - other.x)))
+        angle = math.atan2((self.y - other.y),(self.x - other.x))
+        self.unique.add(angle)
+        return angle
 
     def set_max(self):
         self.max = len(self.unique)
+        return
+    def L2_norm(self,other):
+        math.sqrt((self.x-other.x)**2+(self.y-other.y)**2)
+        return
 
 with open("day10.txt", 'r') as fp:
     galaxy = fp.readlines()
@@ -30,19 +37,28 @@ with open("day10.txt", 'r') as fp:
             if ast == '#':
                 ast_list.append(Asteroid(x,y))
 
+station = None
+detected = -math.inf
+
 for ast in ast_list:
     for other in ast_list:
         if ast != other:
-            ast.slope(other)
+            angle = ast.slope(other)
+
     ast.set_max()
+    if ast.max > detected:
+        detected = ast.max
+        station = ast
 
-detected = -math.inf
-for i in ast_list:
-    if i.max > detected:
-        detected = i.max
-print(detected)
+print(detected) # 299
+print(station) # x_coordinate 29, y_coordinate 26
 
-# # TODO: figure out how to stack into matrix
-# TODO: Find all the planets with hash mark, put it in dictionary
-# Compute slope for each one (super bad time complexity)
-#
+# Our grid is not cartesian plane
+#  [0..............N]
+#  [.               ]
+#  [.               ]
+#  [.               ]
+#  [.               ]
+#  [N               ]
+
+# Fuck it convert to polar coordinates? And set x_coordinate 29, y_coordinate 26 as origin then sort by angle?
