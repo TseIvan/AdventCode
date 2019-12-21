6f62c61cb891d194c3eb5c740b30303e4ab0775a
TseIvan <itse@ualberta.ca>
2019-12-21 01:16:49 -0700

    cont tmr

--- year2019/day10.py
+++ year2019/day10.py
@@ -53,12 +53,28 @@ for ast in ast_list:
 print(detected) # 299
 print(station) # x_coordinate 29, y_coordinate 26
 
-# Our grid is not cartesian plane
-#  [0..............N]
-#  [.               ]
-#  [.               ]
-#  [.               ]
-#  [.               ]
-#  [N               ]
+ast_dict = defaultdict(list)
 
-# Fuck it convert to polar coordinates? And set x_coordinate 29, y_coordinate 26 as origin then sort by angle?
+for other in ast_list:
+    if other != station:
+        station.L2_norm(other)
+        angle = other.slope(station)
+        ast_dict[angle].append(other)
+
+for key in sorted(ast_dict.keys()):
+    print(key, ast_dict[key][0])
+    pass
+# Compute atan2 for North east south west of the point 29,26
+
+print(math.atan2((35 - 26),(30 - 29))) # North of point
+# print(math.atan2((26 - 26),(29 - 30))) # East of point
+# print(math.atan2((26 - 25),(29 - 29))) # South of point
+# print(math.atan2((26 - 26),(29 - 28))) # West of point
+
+# -3.107123552590285 x_coordinate 0, y_coordinate 25
+# -1.5253730473733196 x_coordinate 30, y_coordinate 4
+# 0.0 x_coordinate 37, y_coordinate 26
+# 1.5707963267948966 x_coordinate 29, y_coordinate 29
+# 3.141592653589793 x_coordinate 0, y_coordinate 26
+
+# What if we put into "bins" and sort each "bin" (from pi/2 to 2pi)
