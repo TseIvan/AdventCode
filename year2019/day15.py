8dc316b4c13715b8a3fe148f85184494b480442b
TseIvan <itse@ualberta.ca>
2020-01-10 19:32:03 -0700

    d15 does not enqueue properly

--- year2019/day15.py
+++ year2019/day15.py
@@ -1,36 +1,50 @@
 from IntCode import IntCode, parseFile
 import heapq
 import copy
+dir_dict = {
+    'north': {'change':[0,1], 'input':1},
+    'south': {'change':[0,-1], 'input':2},
+    'west': {'change':[-1,0], 'input':3},
+    'east': {'change':[1,0], 'input':4},
+}
+def updatePos(x,y,dir):
+    global dir_dict
+    x += dir_dict[dir].get('change')[0]
+    y += dir_dict[dir].get('change')[1]
+    return (x,y)
 
 def run():
-    dir_dict = {
-        'north': {'change':[0,1], 'input':1},
-        'south': {'change':[0,-1], 'input':2},
-        'west': {'change':[-1,0], 'input':3},
-        'east': {'change':[1,0], 'input':4},
-    }
+    global dir_dict
     # directions = ['north','south','west','east']
 
-    visited = {(0,0):"█"}
-    program = IntCode(parseFile('day13.txt'))
+    visited = {(0,0):" "}
+    program = IntCode(parseFile('day15.txt'))
     # Initialize heap to store list
     h = list()
     heapq.heapify(h)
 
     # Storage structure -> list [tuple(int x,int y) , class:IntCode program, steps:int, direction:str]
     # Initialize start
-    [heapq.heappush(h,[(0,0),program,0,dir]) for dir in dir_dict.keys()]
+    [heapq.heappush(h,[updatePos(0,0,dir),program,0,dir]) for dir in dir_dict.keys()]
 
     while len(h):
-         # north (1), south (2), west (3), and east (4)
         (x,y),prog,steps,dir = heapq.heappop(h)
-        print((x,y))
-        print(prog)
-        print(steps)
-        print(dir)
-        dupe = program.duplicate() # Create new duplicated instance
-        print(dir_dict[dir].get('input'))
-        # dupe.input(dir_dict[dir].get('input'))
-    return
+        dupe = prog.duplicate() # Create new duplicated instance
+        dupe.input_signal(dir_dict[dir].get('input'))
+        status = dupe.output.pop(0)
+        # print(x,y,status,dir)
+        if status == 2:
+            visited[(x,y)] = "X"
+            print(steps)
+            return(steps)
+        elif status == 1:
+        #     print(x,y,status,dir)
+            visited[(x,y)] = " "
+            for dir in dir_dict.keys():
+                x,y = updatePos(x,y,dir)
+                if (x,y) not in visited.keys():
+                    heapq.heappush(h,[(x,y),dupe.duplicate(),steps+1,dir])
+        elif status == 0:
+            visited[(x,y)] = "█"
 
 run()
